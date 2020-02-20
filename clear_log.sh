e6ca9198126fc3549df113f7a78c47b3ba0e2c6f
Khải Phan <khaiphan9x@gmail.com>
2020-02-20 10:06:48 +0700

    Add files via upload

---
+++ clear_log.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd /var/log
+truncate -s 0 /var/log/*log
+find . -name "*.gz" -type f -delete
+find . -name "*.0" -type f -delete
+find . -name "*.1" -type f -delete
+find . -name "*.log.*" -type f -delete
